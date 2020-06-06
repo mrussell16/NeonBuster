d1e34531e19a29f410de420a959734f11c5ad020
Matthew Russell <m.russell@analyticsengines.com>
2020-06-06 16:55:04 +0100

    Add ball spin

--- actors/Ball/Ball.gd
+++ actors/Ball/Ball.gd
@@ -6,11 +6,20 @@ signal killed_by_killbox(ball)
 
 
 export var movement_speed: float = 400
+export var spin_distance: float = 20
+export var paddle_width: float = 100
 
 
 var velocity: Vector2 = Vector2.ZERO
 var on_paddle: bool = true
 var index_on_player: int = 0
+var half_paddle_width: int = 0
+var spin_start: int = 0
+
+
+func _ready() -> void:
+    half_paddle_width = paddle_width / 2
+    spin_start = half_paddle_width - spin_distance
 
 
 func start_from_paddle(direction: Vector2) -> void:
@@ -33,12 +42,26 @@ func reset(paddle_position: Vector2):
     velocity = Vector2.ZERO
 
 
+func _handle_paddle_collision(collision: KinematicCollision2D):
+    var position_difference = collision.position.x - collision.collider.global_position.x
+    var new_velocity = Vector2(0, -1)
+    if position_difference > spin_start:
+        new_velocity.x = 1 + (position_difference - spin_start) / spin_distance
+    elif position_difference < -spin_start:
+        new_velocity.x = -1 + (position_difference + spin_start) / spin_distance
+    else:
+        new_velocity.x = sign(velocity.x)
+
+    velocity = new_velocity.normalized() * movement_speed
+
+
 func _physics_process(delta: float) -> void:
     var collision: KinematicCollision2D = move_and_collide(velocity * delta)
     if collision:
-        # TODO: Detect if near edge of paddle and apply tweaking to angle
-        # if collision.collider.name == 'Paddle':
-        if abs(collision.normal.x) > 0.1:
-            velocity.x = -velocity.x
-        elif abs(collision.normal.y) > 0.1:
-            velocity.y = -velocity.y
+        if collision.collider.name == 'Paddle':
+            _handle_paddle_collision(collision)
+        else:
+            if abs(collision.normal.x) > 0.1:
+                velocity.x = -velocity.x
+            elif abs(collision.normal.y) > 0.1:
+                velocity.y = -velocity.y
